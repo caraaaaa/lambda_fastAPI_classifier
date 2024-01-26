355cbb6921a02370396d7469c5273c6d852aeec2
caraaaaa <krausert@hotmail.com>
2024-01-26 14:12:36 -0500

    add performance test sample

--- src/utils/test_images.py
+++ src/utils/test_images.py
@@ -8,9 +8,23 @@ image_links = [
         "name": "cat.jpg"
     },
     {
-        "url": "https://media.istockphoto.com/photos/hen-picture-id831791190"
-               "?k=20&m=831791190&s=170667a&w=0&h=pT2buEaMcxE5Wm4hQz-pXl0Zv"
-               "-0DdpSMwpDzVcpcKyo=",
-        "name": "hen.jpg"
+        "url": "https://www.thesprucepets.com/thmb/DRYnRM01YDiFUK1mUIifr5nj60k=/4239x0/filters:no_upscale():strip_icc()/ricky-kharawala-adK3Vu70DEQ-unsplash-0fd4bcb628bd49c88d8a023130132a7f.jpg",
+        "name": "hamster.jpg"
+    },
+    {
+        "url": "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e9/TROPICS_Rocket_Launch_%28KSC-20230507-PH-RLS01_0002-reupload%29_%28cropped%29.jpg/440px-TROPICS_Rocket_Launch_%28KSC-20230507-PH-RLS01_0002-reupload%29_%28cropped%29.jpg",
+        "name": "rocket.jpg"
+    },
+    {
+        "url": "https://i.ebayimg.com/images/g/vlIAAOSwikBcR0nA/s-l1200.webp",
+        "name": "teddy.jpg"
+    },
+    {
+        "url": "https://www.si.com/.image/ar_16:9%2Cc_fill%2Ccs_srgb%2Cfl_progressive%2Cg_xy_center%2Cq_auto:good%2Cw_1200%2Cx_1994%2Cy_1934/MTk4MTQ4NTkzMjI2MzU5OTk4/nike-lebron.jpg",
+        "name": "running_shoes.jpg"
+    },
+    {
+        "url": "https://neurosciencenews.com/files/2023/09/jellyfish-memory-learning-neuroscience.jpg",
+        "name": "jellyfish.jpg"
     }
 ]
--- tests/helpers.py
+++ tests/helpers.py
@@ -1,17 +1,17 @@
 import sys
 import os
 sys.path.insert(0, os.getcwd())
+import random
 
 from src.utils.test_images import image_links
 import json
 from src.schemas.image_schema import Img
 
 def predict_test(client, api_url):
-    sample = Img(img_url=image_links[0]["url"])
+    sample = Img(img_url=random.choice(image_links)['url'])
     headers = {'Accept': 'application/json',
                'Content-Type': 'application/json'}
     res = client.post(api_url,
                       data=json.dumps(sample.dict()),
                       headers=headers)
-
     return res.json()
